959ba4689f2fef742c7e32a5a628d24611a471dc
noelcjr <noelcarrascal@gmail.com>
2017-08-09 09:00:06 -0700

    Lecture 1, and .gitignore added.

---
+++ Lecture_1/Presentation/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+latex Lecture_1.tex
+dvipdf Lecture_1.dvi
---
+++ Lecture_1/bash_test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Executing command line program 1"
+echo "Executing command line program 2"
+echo "Executing command line program 3"
