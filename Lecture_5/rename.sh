80b2ca7257fcc937fcfc4d97380664f1e3d228e4
noelcjr <noelcarrascal@gmail.com>
2017-09-06 00:06:28 -0700

    modified the pdb files from flower.

---
+++ Lecture_5/rename.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+mv s_0_0_0_aa_bb.pdb s_0_0_0_AA_BB.pdb
+mv s_0_270_25_aa_bb.pdb s_0_270_25_AA_BB.pdb
+mv s_0_315_16_aa_bb.pdb s_0_315_16_AA_BB.pdb
+mv s_0_45_8_aa_bb.pdb s_0_45_8_AA_BB.pdb
+mv s_0_90_24_aa_bb.pdb s_0_90_24_AA_BB.pdb
+mv s_135_0_3_aa_bb.pdb s_135_0_3_AA_BB.pdb
+mv s_135_315_19_aa_bb.pdb s_135_315_19_AA_BB.pdb
+mv s_135_45_11_aa_bb.pdb s_135_45_11_AA_BB.pdb
+mv s_180_0_4_aa_bb.pdb s_180_0_4_AA_BB.pdb
+mv s_180_315_20_aa_bb.pdb s_180_315_20_AA_BB.pdb
+mv s_180_45_12_aa_bb.pdb s_180_45_12_AA_BB.pdb
+mv s_225_0_5_aa_bb.pdb s_225_0_5_AA_BB.pdb
+mv s_225_315_21_aa_bb.pdb s_225_315_21_AA_BB.pdb
+mv s_225_45_13_aa_bb.pdb s_225_45_13_AA_BB.pdb
+mv s_270_0_6_aa_bb.pdb s_270_0_6_AA_BB.pdb
+mv s_270_315_22_aa_bb.pdb s_270_315_22_AA_BB.pdb
+mv s_270_45_14_aa_bb.pdb s_270_45_14_AA_BB.pdb
+mv s_315_0_7_aa_bb.pdb s_315_0_7_AA_BB.pdb
+mv s_315_315_23_aa_bb.pdb s_315_315_23_AA_BB.pdb
+mv s_315_45_15_aa_bb.pdb s_315_45_15_AA_BB.pdb
+mv s_45_0_1_aa_bb.pdb s_45_0_1_AA_BB.pdb
+mv s_45_315_17_aa_bb.pdb s_45_315_17_AA_BB.pdb
+mv s_45_45_9_aa_bb.pdb s_45_45_9_AA_BB.pdb
+mv s_90_0_2_aa_bb.pdb s_90_0_2_AA_BB.pdb
+mv s_90_315_18_aa_bb.pdb s_90_315_18_AA_BB.pdb
+mv s_90_45_10_aa_bb.pdb s_90_45_10_AA_BB.pdb
