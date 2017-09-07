5f2acdec75ee7dbae8b8fe7a9c54fa3e0ab31421
noelcjr <noelcarrascal@gmail.com>
2017-09-06 19:57:27 -0700

    Added sald residis to run_tutorial.sh to add Na and CL.

--- Lecture_5/run_tutorial.sh
+++ Lecture_5/run_tutorial.sh
@@ -41,6 +41,20 @@ perl -pi -e "s/PATH/\/home\/CCL_Lectures\/Lecture_5\/"$yourfile"box_setup/g" add
 perl -pi -e "s/INPUT/"$yourfile"r_min3_box/g" add_NaCl.inp
 perl -pi -e "s/OUTPUT/"$yourfile"r_min3_box_ions/g" add_NaCl.inp
 
+#resid 23294 .or. resid 7732 .or. resid 23188 .or. resid 18521 .or. resid 13532 .or. -
+#resid 26196 .or. resid 11922 .or. resid 2797 .or. resid 31311 .or. resid 22681 .or. -
+#resid 30947 .or. resid 27474 .or. resid 28245 .or. resid 2507 .or. resid 1439 .or. -
+#resid 3133 .or. resid 27125 .or. resid 6735 .or. resid 12568 .or. resid 16326 .or. -
+#resid 4105 .or. resid 17784 .or. resid 20966 .or. resid 28651 .or. resid 22830 .or. -
+#resid 5108 .or. resid 5796 .or. resid 21058 .or. resid 22218 .or. resid 24112 .or. -
+#resid 11472 
+
+#resid 9420 .or. resid 27150 .or. resid 16208 .or. resid 29689 .or. resid 11628 .or. -
+#resid 16053 .or. resid 16674 .or. resid 29483 .or. resid 15944 .or. resid 9225 .or. -
+#resid 18921 .or. resid 21926 .or. resid 4376 .or. resid 12951 .or. resid 11388 .or. -
+#resid 389 .or. resid 24358 .or. resid 4103 .or. resid 16723 .or. resid 32512 .or. -
+#resid 8475 .or. resid 33687 .or. resid 19381 .or. resid 31323 .or. resid 18149 .or.
+
 charmm < add_NaCl.inp > add_NaCl.out
 
 cd ..
